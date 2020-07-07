2979e179a02fab9deda169496dc865e5677d836b
Riptide3 <riptide.yzy@gmail.com>
2020-07-07 17:44:50 +0800

    add animation

--- mainwindow.py
+++ mainwindow.py
@@ -187,7 +187,7 @@ class Ui_MainWindow(object):
         font.setFamily("新宋体")
         font.setPointSize(12)
         self.daySpin.setFont(font)
-        self.daySpin.setMaximum(6)
+        self.daySpin.setMaximum(2)
         self.daySpin.setObjectName("daySpin")
         self.timeLimitLabel = QtWidgets.QLabel(self.main)
         self.timeLimitLabel.setGeometry(QtCore.QRect(140, 456, 51, 20))
@@ -239,8 +239,32 @@ class Ui_MainWindow(object):
         self.mapLabel = QtWidgets.QLabel(self.main)
         self.mapLabel.setGeometry(QtCore.QRect(600, -3, 1130, 950))
         self.mapLabel.setText("")
-        self.mapLabel.setPixmap(QtGui.QPixmap("map.png"))
+        self.mapLabel.setPixmap(QtGui.QPixmap("./images/map.png"))
         self.mapLabel.setObjectName("mapLabel")
+
+        self.pathLabel = QtWidgets.QLabel(self.main)
+        self.pathLabel.setGeometry(QtCore.QRect(600, -3, 1130, 950))
+        self.pathLabel.setText("")
+        self.pathLabel.setObjectName("pathLabel")
+        self.painter = QtGui.QPainter()
+        self.pen = QtGui.QPen(QtCore.Qt.red, 1, QtCore.Qt.SolidLine, QtCore.Qt.RoundCap, QtCore.Qt.RoundJoin)
+        self.cityCoordinates = {'澳门': QtCore.QPoint(813, 811), '北京': QtCore.QPoint(812, 361),
+                                '成都': QtCore.QPoint(582, 607), '福州': QtCore.QPoint(920, 704),
+                                '广州': QtCore.QPoint(797, 786), '贵阳': QtCore.QPoint(643, 711),
+                                '哈尔滨': QtCore.QPoint(978, 198), '海口': QtCore.QPoint(721, 878),
+                                '杭州': QtCore.QPoint(917, 606), '合肥': QtCore.QPoint(862, 562),
+                                '呼和浩特': QtCore.QPoint(727, 349), '济南': QtCore.QPoint(845, 443),
+                                '昆明': QtCore.QPoint(549, 748), '拉萨': QtCore.QPoint(309, 605),
+                                '兰州': QtCore.QPoint(579, 478), '南昌': QtCore.QPoint(842, 648),
+                                '南京': QtCore.QPoint(901, 561), '南宁': QtCore.QPoint(679, 803),
+                                '上海': QtCore.QPoint(949, 567), '沈阳': QtCore.QPoint(945, 297),
+                                '石家庄': QtCore.QPoint(789, 409), '台北': QtCore.QPoint(972, 726),
+                                '太原': QtCore.QPoint(750, 422), '天津': QtCore.QPoint(842, 379),
+                                '乌鲁木齐': QtCore.QPoint(291, 252), '武汉': QtCore.QPoint(803, 604),
+                                '西安': QtCore.QPoint(682, 516), '西宁': QtCore.QPoint(534, 461),
+                                '香港': QtCore.QPoint(784, 816), '银川': QtCore.QPoint(625, 409),
+                                '长春': QtCore.QPoint(967, 245), '长沙': QtCore.QPoint(774, 663),
+                                '郑州': QtCore.QPoint(776, 500), '重庆': QtCore.QPoint(637, 630)}
         self.tabWidget.addTab(self.main, "")
         self.log = QtWidgets.QWidget()
         self.log.setObjectName("log")
@@ -297,6 +321,10 @@ class Ui_MainWindow(object):
         self.logTextBrowser = QtWidgets.QTextBrowser(self.log)
         self.logTextBrowser.setMaximumSize(QtCore.QSize(800, 16777215))
         self.logTextBrowser.setObjectName("logTextBrowser")
+        font = QtGui.QFont()
+        font.setFamily("新宋体")
+        font.setPointSize(13)
+        self.logTextBrowser.setFont(font)
         self.logGridLayout.addWidget(self.logTextBrowser, 1, 1, 1, 1)
         self.logGridLayout.setColumnStretch(0, 1)
         self.logGridLayout.setColumnStretch(1, 6)
@@ -312,7 +340,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         self.tabWidget.setCurrentIndex(0)
-        # my
+        # 连接槽函数
         self.daySpin.setEnabled(False)
         self.hourSpin.setEnabled(False)
         self.strategyRadioButton1.setChecked(True)
@@ -320,7 +348,6 @@ class Ui_MainWindow(object):
         self.strategyRadioButton2.toggled.connect(self.hourSpin.setEnabled)
         self.startButton.clicked.connect(self.startButton_clicked)
         self.stateSearchButton.clicked.connect(self.stateSearchButton_clicked)
-        # my
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
@@ -391,14 +418,25 @@ class Ui_MainWindow(object):
 
     # 定时任务
     def timed_tasks(self):
+        self.timer.stop()
         self.time_now += self.one_hour
+        states = self.sim.get_all_states(self.time_now)
+        self.vehicle_move(states)
+        self.log_update(states)
+        self.draw_path(self.sim.get_all_routes())
         self.timeLabel.setText(self.time_now.strftime("%Y{y}%m{m}%d{d}%H{h}").format(y="年", m="月", d="日", h="时"))
-        self.sim.get_all_state(self.time_now)
+        self.logTextBrowser.append(self.time_now.strftime("%Y{y}%m{m}%d{d}%H{h}").format(y="年", m="月", d="日", h="时"))
+        self.timer.start()
 
+    # 启停定时器时的一次性定时任务
     def one_time_tasks(self):
         self.time_now += self.one_hour
+        states = self.sim.get_all_states(self.time_now)
+        self.vehicle_move(states)
+        self.log_update(states)
+        self.draw_path(self.sim.get_all_routes())
         self.timeLabel.setText(self.time_now.strftime("%Y{y}%m{m}%d{d}%H{h}").format(y="年", m="月", d="日", h="时"))
-        self.sim.get_all_state(self.time_now)
+        self.logTextBrowser.append(self.time_now.strftime("%Y{y}%m{m}%d{d}%H{h}").format(y="年", m="月", d="日", h="时"))
         self.timer.start()
 
     # 显示系统为旅客规划的路线
@@ -412,9 +450,7 @@ class Ui_MainWindow(object):
         if len(route) > 0:
             text += f'为您规划的最佳路线如下：\n'
             for part in route:
-                if part['origin'] == part['destination']:
-                    continue
-                else:
+                if part['origin'] != part['destination']:
                     if part['departure_time'] < prev:
                         d_date += one_day
                     text += f"{d_date.strftime('%m')}月{d_date.strftime('%d')}日{part['departure_time']:02}时 - "
@@ -422,7 +458,7 @@ class Ui_MainWindow(object):
                         d_date += one_day
                     text += f"{d_date.strftime('%m')}月{d_date.strftime('%d')}日{part['arrival_time']:02}时  " \
                             f"{part['origin']} --> {part['destination']}  {E2C[part['type']]}\n"
-            text += f"当前路线的风险总值为：{risk}"
+            text += f"该路线的风险总值为：{risk}"
         else:
             text += f'非常抱歉，基于您当前的选择，系统无法为您找到合适的路线。'
 
@@ -438,33 +474,39 @@ class Ui_MainWindow(object):
         self.timer.stop()
         name = self.nameLineEdit.text()
         ID = self.idLineEdit1.text()
+        origin = self.originCombo.currentText()
+        destination = self.destinationCombo.currentText()
+        limit = self.strategyRadioButton2.isChecked()
+        if limit:
+            time_limit = int(self.daySpin.text()[:-1]) * 24 + int(self.hourSpin.text()[:-1])
+        else:
+            time_limit = 2 * 24  # 修改此处更改默认限时
         if name == '' or ID == '':
             QtWidgets.QMessageBox.warning(self.centralwidget, '注意', '姓名和身份证号不能为空！',
                                           QtWidgets.QMessageBox.Ok, QtWidgets.QMessageBox.Ok)
-        elif self.originCombo.currentText() == self.destinationCombo.currentText():
+        elif origin == destination:
             QtWidgets.QMessageBox.warning(self.centralwidget, '注意', '出发地和目的地不能相同！',
                                           QtWidgets.QMessageBox.Ok, QtWidgets.QMessageBox.Ok)
+        elif time_limit <= 0:
+            QtWidgets.QMessageBox.warning(self.centralwidget, '注意', '时间限制必须大于零！',
+                                          QtWidgets.QMessageBox.Ok, QtWidgets.QMessageBox.Ok)
         else:
-            origin = self.originCombo.currentText()
-            destination = self.destinationCombo.currentText()
-            limit = self.strategyRadioButton2.isChecked()
-            if limit:
-                time_limit = int(self.daySpin.text()[:-1]) * 24 + int(self.hourSpin.text()[:-1])
-            else:
-                time_limit = 4 * 24  # TODO: 修改此处更改默认限时
+
             traveler = {'name': name, 'ID': ID, 'origin': origin, 'destination': destination,
                         'departure_date': self.time_now, 'time_limit': time_limit}
             self.nameLineEdit.clear()
             self.idLineEdit1.clear()
             self.sim.new_traveler(traveler)
             self.show_route(name, ID)
-            print(f'{name}: {self.sim.get_state(ID, self.time_now)}')
+            self.draw_path(self.sim.get_all_routes())
+            state = self.sim.get_state(ID, self.time_now)
+            self.animation = self.anima(state, remainingTime - 25)
+            self.animation.start()
+            self.logTextBrowser.append(f"{name}(ID:{ID})开始旅行")
         self.timer.singleShot(remainingTime, self.one_time_tasks)
 
     # 获取某一旅客状态的按钮被点击
     def stateSearchButton_clicked(self):
-        remainingTime = self.timer.remainingTime()
-        self.timer.stop()
         ID = self.stateSearchIDLineEdit.text()
         if ID == '':
             QtWidgets.QMessageBox.warning(self.centralwidget, '注意', '身份证号不能为空！',
@@ -477,8 +519,77 @@ class Ui_MainWindow(object):
             self.stateSearchTextBrowser.setFont(font)
             E2C = {'BUS': '汽车', 'TRAIN': '火车', 'AIRPLANE': '飞机'}
             if state['type'] == 'STAY':
-                text = f"旅客当前停留在{state['origin']}\n"
+                text = f"旅客当前停留在{state['origin']}"
             else:
-                text = f"旅客当前处于{state['origin']}前往{state['destination']}的{E2C[state['type']]}上\n"
+                text = f"旅客当前处于{state['origin']}前往{state['destination']}的{E2C[state['type']]}上"
             self.stateSearchTextBrowser.setText(text)
-            self.timer.singleShot(remainingTime, self.timer.start)
+            self.logTextBrowser.append(f"查询旅客(ID:{ID})状态，" + text)
+
+    # 在地图上画出路线
+    def draw_path(self, routes):
+        pix = QtGui.QPixmap(1130, 950)
+        pix.fill(QtCore.Qt.transparent)
+        path = QtGui.QPainterPath()
+        for route in routes:
+            path.moveTo(self.cityCoordinates[route[0]['origin']])
+            for part in route:
+                if part['origin'] != part['destination']:
+                    path.lineTo(self.cityCoordinates[part['destination']])
+        self.painter.begin(pix)
+        self.painter.setRenderHints(QtGui.QPainter.Antialiasing | QtGui.QPainter.SmoothPixmapTransform)
+        self.painter.setPen(self.pen)
+        self.painter.drawPath(path)
+        self.painter.end()
+        self.pathLabel.setPixmap(pix)
+
+    # 更新系统日志
+    def log_update(self, states):
+        E2C = {'BUS': '汽车', 'TRAIN': '火车', 'AIRPLANE': '飞机'}
+        if len(states) > 0:
+            for state in states:
+                if state['type'] == 'ARRIVAL':
+                    self.logTextBrowser.append(f"{state['name']}(ID:{state['ID']})到达目的地{state['destination']}")
+                elif state['type'] == 'STAY':
+                    self.logTextBrowser.append(f"{state['name']}(ID:{state['ID']})当前停留在{state['origin']}")
+                else:
+                    self.logTextBrowser.append(f"{state['name']}(ID:{state['ID']})当前处于"
+                                               f"{state['origin']}前往{state['destination']}的{E2C[state['type']]}上")
+
+    # 交通工具移动动画
+    def anima(self, state, duration):
+        initPoint = QtCore.QPoint(600 - 15, -3 - 15)
+        vehicleLabel = QtWidgets.QLabel(self.main)
+        vehicleLabel.setGeometry(QtCore.QRect(600, -3, 30, 30))
+        vehicleLabel.setText("")
+        vehicleLabel.setObjectName("vehicleLabel")
+        vehicleLabel.setPixmap(QtGui.QPixmap("./images/" + state["type"] + ".png"))
+        vehicleLabel.show()
+        animation = QtCore.QPropertyAnimation(vehicleLabel, b"pos")
+        if state['arrival_time'] > state['departure_time']:
+            total_time = state['arrival_time'] - state['departure_time']
+            time_diff = int(self.time_now.strftime("%H")) - state['departure_time']
+        else:
+            total_time = state['arrival_time'] + 24 - state['departure_time']
+            time_now = int(self.time_now.strftime("%H"))
+            if time_now > state['departure_time']:
+                time_diff = time_now - state['departure_time']
+            else:
+                time_diff = time_now + 24 - state['departure_time']
+        ratio = time_diff / total_time
+        total_dis = self.cityCoordinates[state["destination"]] - self.cityCoordinates[state["origin"]]
+        start_point = self.cityCoordinates[state["origin"]] + ratio * total_dis + initPoint
+        end_point = start_point + 1 / total_time * total_dis
+        animation.setStartValue(start_point)
+        animation.setEndValue(end_point)
+        animation.setDuration(duration)
+        animation.finished.connect(lambda: vehicleLabel.hide())
+        return animation
+
+    # 多旅客动画
+    def vehicle_move(self, states):
+        states = [s for s in states if s["type"] != "ARRIVAL"]
+        if len(states) > 0:
+            self.animations = QtCore.QParallelAnimationGroup()
+            for i, state in enumerate(states):
+                self.animations.addAnimation(self.anima(state, 10 * 1000 - 25))
+            self.animations.start()
--- simulator.py
+++ simulator.py
@@ -1,7 +1,4 @@
 import traveler
-import simtimer
-import threading
-from PyQt5 import QtCore
 
 
 class Simulator:
@@ -9,28 +6,36 @@ class Simulator:
         self.travelers = {}
 
     # 获取所有旅客的当前状态
-    def get_all_state(self, time_now):
-        print()
-        print(time_now)
+    def get_all_states(self, time_now):
+        states = []
         if len(self.travelers) > 0:
             for t in list(self.travelers.values()):
                 time_diff = time_now - t.departure_date
                 time_diff = time_diff.days * 24 + time_diff.seconds // 3600
-                # if time_diff < len(t.states):
-                #     # state = t.get_state(time_diff)
-                #     # print(f'{t.name}从{t.origin}出发
-                #     print()
                 if time_diff >= len(t.states):
                     self.travelers.pop(t.ID)
-                    print(f'{t.name}到达目的地{t.destination}')
+                    state = {"ID": t.ID, "name": t.name, "type": "ARRIVAL", "destination": t.destination}
+                    states.append(state)
                     continue
-                print(f'{t.name}: {t.get_state(time_diff)}')
+                else:
+                    state = t.get_state(time_diff)
+                    state.update({"ID": t.ID, "name": t.name})
+                    states.append(state)
+        return states
 
+    # 获取当前存在的所有旅行路线
+    def get_all_routes(self):
+        routes = []
+        for traveler in self.travelers.values():
+            routes.append(traveler.route)
+        return routes
+
+    # 添加新的旅客
     def new_traveler(self, t):
         newTraveler = traveler.Traveler(t['name'], t['ID'], t['origin'], t['destination'], t['departure_date'], t['time_limit'])
         self.travelers.update({t['ID']: newTraveler})
 
-    # 获取系统规划出的旅行路线
+    # 根据ID获取系统为某一旅客规划出的旅行路线
     def get_plan(self, ID):
         return self.travelers[ID].departure_date, self.travelers[ID].risk, self.travelers[ID].route
 
