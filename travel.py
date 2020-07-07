8176c8e155698afd8d110e6e37defbcf874c5b55
Riptide3 <riptide.yzy@gmail.com>
2020-07-07 18:03:43 +0800

    rename python file & add pictures and icon

--- travel.py
+++ travel.py
@@ -1,10 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication
+from PyQt5 import QtGui
 import mainwindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     MainWindow = mainwindow.MainWindow()
+    MainWindow.setWindowIcon(QtGui.QIcon("./images/travel.png"))
     ui = mainwindow.Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.showMaximized()
